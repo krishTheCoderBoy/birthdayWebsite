eefd38115d20678d4ffe4be0ffb8b3c5e8c225a6
krishnendu <kd743452@gmail.com>
2025-04-20 09:38:44 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
  var b=prompt("Enter password");
-  	if(a== "krishnendu" && b=="10-10-2002")
+  	if(a== "krishnendu" && b=="10102002")
   	{
   	    window.location.href="start.html";
   	}
